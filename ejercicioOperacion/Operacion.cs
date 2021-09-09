48daef7b2cd0ec528d6cc402c86d6e5901b4a2e5
francozufia <zufia96@gmail.com>
2021-09-09 13:20:39 -0300

    Entrega ejercico operaciones estaticas

---
+++ ejercicioOperacion/Operacion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioOperacion
+{
+    static class Operacion
+    {
+        private static int n1 , n2;
+        static public void pedirDatos()
+        {
+            Console.Write("Ingrese el primer numero : ");
+            n1 = Int32.Parse(Console.ReadLine());
+            Console.Write("Ingrese el segundo numero : ");
+            n2 = Int32.Parse(Console.ReadLine());
+        }
+       static public int suma()
+        {
+            return n1 + n2;
+        }
+
+        static public int resta()
+        {
+            return n1 - n2;
+        }
+    }
+}
---
+++ ejercicioOperacion/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ejercicioOperacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operacion.pedirDatos();
+            Console.WriteLine("La suma es : " + Operacion.suma());
+            Console.WriteLine("La resta es : " + Operacion.resta());
+        }
+    }
+}
